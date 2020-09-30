542af21ad0ed29c16965577ecbc24715fa8fa6d5
Kevin <floreskevin51398@gmail.com>
2020-09-30 06:35:05 -0500

    Begining to add commands, will grow this into bigger file so we might need to consider building this in a OOP structure. ~kev

--- .idea/main.js
+++ .idea/main.js
@@ -2,8 +2,27 @@ const Discord = require('discord.js');
 
 const client = new Discord.Client();
 
+/// prefix of commands feel free to change this
+const prefix = '+'
+
+
 client.once('ready' , () => {
     console.log('Jarvis is Online!');
+});
+
+client.on('message', message =>{
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+
+    /// Command block begins here
+    if(command === 'ping'){
+        message.channel.send('pong!');
+    }else if (command == 'faq' || command == 'FAQ'){
+        message.channel.send("")
+    }
 });
 
 
@@ -26,4 +45,4 @@ client.once('ready' , () => {
 
 
 
-client.login('NzYwODE5NDIxNjk4MDY0NDA0.X3RmTQ.BAPHg8OPV-nlhxnhP5xgAhhyrlo');
+client.login('NzYwODE5NDIxNjk4MDY0NDA0.X3RmTQ.H5AkPod_08zk5rDwji57ExT4ryM');
