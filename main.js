46ad78e5a5658b5eb11b01252ce585d551c44044
rckoepke <koepke@gmail.com>
2020-10-08 06:29:57 -0500

    making GitHub's superlinter happy....

--- main.js
+++ main.js
@@ -1,85 +1,81 @@
-const Discord = require('discord.js');
-const fs = require('fs');
+const Discord = require('discord.js')
+const fs = require('fs')
 
-const client = new Discord.Client();
+const client = new Discord.Client()
 
 /// prefix of commands feel free to change this
 const prefix = '+'
 
-//requiring path and fs modules
-const path = require('path');
+// requiring path and fs modules
+const path = require('path')
 
-client.commands = new Discord.Collection();
+client.commands = new Discord.Collection()
 
 //joining path of directory
 
-const directoryPath = path.join(__dirname, 'commands');
-//passsing directoryPath and callback function
+const directoryPath = path.join(__dirname, 'commands')
+// passing directoryPath and callback function
 fs.readdir(directoryPath, function (err, files) {
-    //handling error
-    if (err) {
-        return console.log('Unable to scan directory: ' + err);
+// handling error
+if (err) {
+  return console.log('Unable to scan directory: ' + err)
+}
+// listing all files using forEach
+files.forEach(function (file) {
+  // Do whatever you want to do with the file
+  const command = require(`./commands/${file}`)
+  client.commands.set(command.name, command)
+
+  console.log(file)
+})
+})
+;
+
+
+client.once('ready', () => {
+  console.log('Jarvis is Online!')
+
+
+})
+
+client.on('message', message => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return
+
+  const args = message.content.slice(prefix.length).split(/ +/)
+  const command = args.shift().toLowerCase()
+
+
+  // Command block begins here
+  client.commands.some(function (cmd) {
+
+    if (command == cmd.name) {
+      cmd.execute(message)
+      return true;
+
     }
-    //listing all files using forEach
-    files.forEach(function (file) {
-        // Do whatever you want to do with the file
-        const command = require(`./commands/${file}`);
-        client.commands.set(command.name, command);
-
-        console.log(file);
-    });
-});
-
-
-
-
-
-client.once('ready' , () => {
-    console.log('Jarvis is Online!');
-
-    
-});
-
-client.on('message', message =>{
-    if(!message.content.startsWith(prefix) || message.author.bot) return;
-    
-    const args = message.content.slice(prefix.length).split(/ +/);
-    const command = args.shift().toLowerCase();
-
-
-    /// Command block begins here
-    client.commands.some(function(cmd) {
-
-        if (command == cmd.name)
-        {
-            cmd.execute(message);
-            return true;
-
-        }
-    });
-});
+  })
+})
 
 client.on('messageReactionAdd', async (reaction, user) => {
-    // When we receive a reaction we check if the reaction is partial or not
-    if (reaction.partial) await reaction.fetch();
-    if (user.bot) return;
-    if (!reaction.message.guild) return;
+  // When we receive a reaction we check if the reaction is partial or not
+  if (reaction.partial) await reaction.fetch()
+  if (user.bot) return
+  if (!reaction.message.guild) return
 
-    // Get rules channel and message information for rules interaction
-    fs.readFile(__dirname + '/data/rules.txt', 'utf8', function(err, data){
+  // Get rules channel and message information for rules interaction
+  fs.readFile(__dirname + '/data/rules.txt', 'utf8', function (err, data) {
 
-        info = data.split('\n');
-        
-        if (reaction.message.channel == info[0] && reaction.message.id == info[1] && reaction.emoji.name == "👍")
-        {
-            reaction.message.channel.send(`Thank you for reacting, ${user}`);
-        }
+    info = data.split('\n')
+
+    if (reaction.message.channel == info[0] && reaction.message.id == info[1] && reaction.emoji.name == "👍") {
+      reaction.message.channel.send(`Thank you for reacting, ${user}`)
+    }
 
 
-    }); 
+  })
 
-});
+})
 
-const bot_token = process.env.NARVIS;
+const bot_token = process.env.NARVIS
 //console.log(bot_token);
-client.login(bot_token);
+client.login(bot_token)
