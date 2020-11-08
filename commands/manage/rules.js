21193f40fae59c6df5fab4b5cdb0178f59fef5bd
rckoepke <koepke@gmail.com>
2020-11-08 08:45:54 -0600

    yet another path error...

--- commands/manage/rules.js
+++ commands/manage/rules.js
@@ -27,7 +27,7 @@ module.exports = {
                 let info = id + "\n" + role;
                 console.log("rules")
                 console.log(__dirname)
-                fs.writeFile(__dirname + '../../data/rules.txt', info, (err) => {
+                fs.writeFile(__dirname + '/../../data/rules.txt', info, (err) => {
                     if (err) throw err;
                     console.log('The file has been saved!');
                   });
