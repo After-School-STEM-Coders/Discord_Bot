d2baec8440ce29839457a71d2a2b24c8e7c84d20
Ross Koepke <koepke@gmail.com>
2020-12-28 12:41:05 -0600

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -3,5 +3,6 @@ if pgrep -f "node.*loggingbranch"
  then kill "$(pgrep -f "node.*loggingbranch")"
 fi
 sleep 5
-node ~/Discord_Bot/loggingbranch/main.js 2>&1 | multilog t s1048576 ./logs/ 2>&1 &
+node ~/Discord_Bot/loggingbranch/main.js 2>&1 | multilog t s1048576 ~/Discord_Bot/loggingbranch/logs/ 2>&1 &
 
+cd ../
