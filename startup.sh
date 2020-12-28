1eb79ef4fda8d68723f786fd1c9105a9acf36772
rckoepke <koepke@gmail.com>
2020-12-27 22:53:49 -0600

    Give roles_branch its own directory on the cloud VM

--- startup.sh
+++ startup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-if pgrep -f "node.*COSCY"
- then kill "$(pgrep -f "node.*COSCY")"
+if pgrep -f "node.*roles_branch_bot"
+ then kill "$(pgrep -f "node.*roles_branch_bot")"
 fi
 sleep 5
 node ~/Discord_Bot/COSCYBOT/main.js &
