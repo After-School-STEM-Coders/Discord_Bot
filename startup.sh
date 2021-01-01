c06bb80a79de5b6f8a7976fb3c1db5a6d204f4d8
rckoepke <koepke@gmail.com>
2021-01-01 02:45:14 -0600

    Fix startup script to match github actions yml

--- startup.sh
+++ startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-pgrep -f "node.*rulesbranch" | while read -r line
+pgrep -f "node.*rules_branch" | while read -r line
 do
   kill $line
 done
-mkdir -p ~/Discord_Bot/rulesbranch/logs/
-node ~/Discord_Bot/rulesbranch/main.js 2>&1 | multilog t s1048576 ~/Discord_Bot/rulesbranch/logs/ 2>&1 &
+mkdir -p ~/Discord_Bot/rules_branch/logs/
+node ~/Discord_Bot/rules_branch/main.js 2>&1 | multilog t s1048576 ~/Discord_Bot/rules_branch/logs/ 2>&1 &
