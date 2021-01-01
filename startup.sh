d90a6e650088d57932eee594baa8ea7fbf94c612
rckoepke <koepke@gmail.com>
2021-01-01 02:47:42 -0600

    fix startup script to create ./logs/ if it does not yet exist.

--- startup.sh
+++ startup.sh
@@ -3,4 +3,5 @@ pgrep -f "node.*COSCY" | while read -r line
 do
   kill $line
 done
+mkdir -p ~/Discord_Bot/COSCYBOT/logs/
 node ~/Discord_Bot/COSCYBOT/main.js 2>&1 | multilog t s1048576 ~/Discord_Bot/COSCYBOT/logs/ 2>&1 &
