71eeea701b4859da9730b59cddfe8b9118432a03
rckoepke <koepke@gmail.com>
2021-01-01 02:29:05 -0600

    Create branch for rules command. Currently not yet working, so removing from master branch.

--- startup.sh
+++ startup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-pgrep -f "node.*COSCY" | while read -r line
+pgrep -f "node.*rulesbranch" | while read -r line
 do
   kill $line
 done
-node ~/Discord_Bot/COSCYBOT/main.js 2>&1 | multilog t s1048576 ~/Discord_Bot/COSCYBOT/logs/ 2>&1 &
+node ~/Discord_Bot/rulesbranch/main.js 2>&1 | multilog t s1048576 ~/Discord_Bot/rulesbranch/logs/ 2>&1 &
