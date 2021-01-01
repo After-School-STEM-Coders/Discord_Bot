ec0e4a1b400e254d3752b804bdbe2f48aab97719
rckoepke <koepke@gmail.com>
2021-01-01 02:34:43 -0600

    Fix startup script to create logs directory if it does not yet exist.

--- startup.sh
+++ startup.sh
@@ -3,4 +3,5 @@ pgrep -f "node.*rulesbranch" | while read -r line
 do
   kill $line
 done
+mkdir -p ~/Discord_Bot/rulesbranch/logs/
 node ~/Discord_Bot/rulesbranch/main.js 2>&1 | multilog t s1048576 ~/Discord_Bot/rulesbranch/logs/ 2>&1 &
